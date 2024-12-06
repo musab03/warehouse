003acdc5e1d12c4bf5e6b43bfe54fbe9a8c6d3cd
Rizwan <syed.51037@iqra.edu.pk>
2024-12-07 03:11:59 +0500

    lklsd

--- src/components/FAQ.tsx
+++ src/components/FAQ.tsx
@@ -3,7 +3,7 @@
 import React, { useEffect, useState } from 'react'
 import AOS from "aos";
 import "aos/dist/aos.css";
-import Image from 'next/image';
+
 
 function FAQ() {
   // State to keep track of which accordion is open
